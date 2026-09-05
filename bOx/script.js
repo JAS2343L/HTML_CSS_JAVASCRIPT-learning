61cbb84d34759eb2c8d1206c06162fdab695f6ba
JAS2343L <js9404107@gmail.com>
2026-09-05 16:57:03 +0530

    noth

--- bOx/script.js
+++
@@ -1,9 +0,0 @@
-let button = document.getElementById("ba");
-button.onclick = abcd;
-
-function abcd(){
-    alert("hello");
-
-    let boxx = document.querySelector(".boox")
-    boxx.style.backgroundColor = "#4a154b"
-}
